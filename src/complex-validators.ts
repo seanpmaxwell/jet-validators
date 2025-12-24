229f4103cec7d2f9c16a8ccf7a2cab07207f713f
Sean Maxwell <spmaxwell7@gmail.com>
2025-12-24 14:05:36 -0500

    refactor cont

--- src/complex-validators.ts
+++ src/complex-validators.ts
@@ -22,14 +22,18 @@ type ResolveMods<T, O extends boolean, N extends boolean, A extends boolean> =
                               isInArray
 ******************************************************************************/
 
-export const isInArray = <T extends readonly unknown[]>(arg: T) =>
-  isInArrayHelper<T, false, false>(arg, false, false);
-export const isOptionalInArray = <T extends readonly unknown[]>(arg: T) =>
-  isInArrayHelper<T, true, false>(arg, true, false);
-export const isNullableInArray = <T extends readonly unknown[]>(arg: T) =>
-  isInArrayHelper<T, false, true>(arg, false, true);
-export const isNullishInArray = <T extends readonly unknown[]>(arg: T) =>
-  isInArrayHelper<T, true, true>(arg, true, true);
+export function isInArray<T extends readonly unknown[]>(arg: T) {
+  return isInArrayHelper<T, false, false>(arg, false, false);
+}
+export function isOptionalInArray<T extends readonly unknown[]>(arg: T) {
+  return isInArrayHelper<T, true, false>(arg, true, false);
+}
+export function isNullableInArray<T extends readonly unknown[]>(arg: T) {
+  return isInArrayHelper<T, false, true>(arg, false, true);
+}
+export function isNullishInArray<T extends readonly unknown[]>(arg: T) {
+  return isInArrayHelper<T, true, true>(arg, true, true);
+}
 
 /**
  * Is an item in an array.
@@ -62,49 +66,33 @@ function isInArrayHelper<
                               isInRange
 ******************************************************************************/
 
-type TRangeParam = number | [number] | [];
-type TRangeFn = (arg: number) => boolean;
+type RangeParam = number | [number] | [];
+type isInRangeFn = (arg: number) => boolean;
 
-export const isInRange = isInRangeHelper<false, false, false>(
-  false,
-  false,
-  false,
-);
-export const isOptionalInRange = isInRangeHelper<true, false, false>(
-  true,
-  false,
-  false,
-);
-export const isNullableInRange = isInRangeHelper<false, true, false>(
-  false,
-  true,
-  false,
-);
-export const isNullishInRange = isInRangeHelper<true, true, false>(
-  true,
-  true,
-  false,
-);
-export const isInRangeArray = isInRangeHelper<false, false, true>(
-  false,
-  false,
-  true,
-);
-export const isOptionalInRangeArray = isInRangeHelper<true, false, true>(
-  true,
-  false,
-  true,
-);
-export const isNullableInRangeArray = isInRangeHelper<false, true, true>(
-  false,
-  true,
-  true,
-);
-export const isNullishInRangeArray = isInRangeHelper<true, true, true>(
-  true,
-  true,
-  true,
-);
+export function isInRange(min: RangeParam, max: RangeParam) {
+  return isInRangeHelper<false, false, false>(false, false, false, min, max);
+}
+export function isOptionalInRange(min: RangeParam, max: RangeParam) {
+  return isInRangeHelper<true, false, false>(true, false, false, min, max);
+}
+export function isNullableInRange(min: RangeParam, max: RangeParam) {
+  return isInRangeHelper<false, true, false>(false, true, false, min, max);
+}
+export function isNullishInRange(min: RangeParam, max: RangeParam) {
+  return isInRangeHelper<true, true, false>(true, true, false, min, max);
+}
+export function isInRangeArray(min: RangeParam, max: RangeParam) {
+  return isInRangeHelper<false, false, true>(false, false, true, min, max);
+}
+export function isOptionalInRangeArray(min: RangeParam, max: RangeParam) {
+  return isInRangeHelper<true, false, true>(true, false, true, min, max);
+}
+export function isNullableInRangeArray(min: RangeParam, max: RangeParam) {
+  return isInRangeHelper<false, true, true>(false, true, true, min, max);
+}
+export function isNullishInRangeArray(min: RangeParam, max: RangeParam) {
+  return isInRangeHelper<true, true, true>(true, true, true, min, max);
+}
 
 /**
  * Range will always determine if a number is >= the min and <= the max.
@@ -118,35 +106,32 @@ function isInRangeHelper<
   optional: boolean,
   nullable: boolean,
   isArr: boolean,
-): (min: TRangeParam, max: TRangeParam) => (arg: unknown) => arg is Ret {
-  return (
-    min: TRangeParam,
-    max: TRangeParam,
-  ): ((arg: unknown) => arg is Ret) => {
-    const rangeFn = setupIsInRangeInternalFn(min, max);
-    const validator = (arg: unknown): arg is Ret => {
-      if (arg === undefined) {
-        return optional;
-      }
-      if (arg === null) {
-        return nullable;
-      }
-      if (isArr) {
-        return (
-          Array.isArray(arg) &&
-          arg.every((item) => isInRangeCheckType(item, rangeFn))
-        );
-      }
-      return isInRangeCheckType(arg, rangeFn);
-    };
-    return markSafe(validator);
+  min: RangeParam,
+  max: RangeParam,
+): (arg: unknown) => arg is Ret {
+  const rangeFn = setupIsInRangeInternalFn(min, max);
+  const validator = (arg: unknown): arg is Ret => {
+    if (arg === undefined) {
+      return optional;
+    }
+    if (arg === null) {
+      return nullable;
+    }
+    if (isArr) {
+      return (
+        Array.isArray(arg) &&
+        arg.every((item) => isInRangeCheckType(item, rangeFn))
+      );
+    }
+    return isInRangeCheckType(arg, rangeFn);
   };
+  return markSafe(validator);
 }
 
 /**
  * Core logic for is array function.
  */
-function isInRangeCheckType(arg: unknown, rangeFn: TRangeFn): boolean {
+function isInRangeCheckType(arg: unknown, rangeFn: isInRangeFn): boolean {
   if (isString(arg)) {
     if (isValidNumber(arg)) {
       arg = Number(arg);
@@ -164,9 +149,9 @@ function isInRangeCheckType(arg: unknown, rangeFn: TRangeFn): boolean {
  * Initialize inRange function.
  */
 function setupIsInRangeInternalFn(
-  min: TRangeParam,
-  max: TRangeParam,
-): TRangeFn {
+  min: RangeParam,
+  max: RangeParam,
+): isInRangeFn {
   // arg >= min && arg <= max
   if (
     Array.isArray(min) &&
@@ -215,14 +200,18 @@ function setupIsInRangeInternalFn(
                               isKeyOf
 ******************************************************************************/
 
-export const isKeyOf = <T extends object>(arg: T) =>
-  isKeyOfHelper<T, false, false>(arg, false, false);
-export const isOptionalKeyOf = <T extends object>(arg: T) =>
-  isKeyOfHelper<T, true, false>(arg, true, false);
-export const isNullableKeyOf = <T extends object>(arg: T) =>
-  isKeyOfHelper<T, false, true>(arg, false, true);
-export const isNullishKeyOf = <T extends object>(arg: T) =>
-  isKeyOfHelper<T, true, true>(arg, true, true);
+export function isKeyOf<T extends object>(arg: T) {
+  return isKeyOfHelper<T, false, false>(arg, false, false);
+}
+export function isOptionalKeyOf<T extends object>(arg: T) {
+  return isKeyOfHelper<T, true, false>(arg, true, false);
+}
+export function isNullableKeyOf<T extends object>(arg: T) {
+  return isKeyOfHelper<T, false, true>(arg, false, true);
+}
+export function isNullishKeyOf<T extends object>(arg: T) {
+  return isKeyOfHelper<T, true, true>(arg, true, true);
+}
 
 /**
  * See if something is a key of an object.
@@ -259,14 +248,18 @@ function isKeyOfHelper<
 
 export type ValueOf<T extends object> = T[keyof T];
 
-export const isValueOf = <T extends object>(arg: T) =>
-  isValueOfHelper<T, false, false>(arg, false, false);
-export const isOptionalValueOf = <T extends object>(arg: T) =>
-  isValueOfHelper<T, true, false>(arg, true, false);
-export const isNullableValueOf = <T extends object>(arg: T) =>
-  isValueOfHelper<T, false, true>(arg, false, true);
-export const isNullishValueOf = <T extends object>(arg: T) =>
-  isValueOfHelper<T, true, true>(arg, true, true);
+export function isValueOf<T extends object>(arg: T) {
+  return isValueOfHelper<T, false, false>(arg, false, false);
+}
+export function isOptionalValueOf<T extends object>(arg: T) {
+  return isValueOfHelper<T, true, false>(arg, true, false);
+}
+export function isNullableValueOf<T extends object>(arg: T) {
+  return isValueOfHelper<T, false, true>(arg, false, true);
+}
+export function isNullishValueOf<T extends object>(arg: T) {
+  return isValueOfHelper<T, true, true>(arg, true, true);
+}
 
 /**
  * See if something is a value in an object.
